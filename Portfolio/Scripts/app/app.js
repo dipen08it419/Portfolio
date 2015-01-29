3eccea74d71ead805e20f86db68d8ac8f3c5dbe0
Dipen Shah <dipen08it419@gmail.com>
2015-01-29 04:30:55 -0800

    Added priivacy policy and terms of use.

---
+++ Portfolio/Scripts/app/app.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../lib/angular.js" />
+
+var app = angular.module('portfolioApp', ['ngRoute'])
+            .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+                $routeProvider.when('/', {
+                    templateUrl: '/partials/home.html'
+                }).when('/about', {
+                    templateUrl: '/partials/about.html'
+                }).when('/contact', {
+                    templateUrl: '/partials/contact.html'
+                }).when('/projects', {
+                    templateUrl: '/partials/projects.html'
+                }).when('/services', {
+                    templateUrl: '/partials/services.html'
+                })
+                .when('/termsOfUse', {
+                    templateUrl: '/partials/terms.html'
+                })
+                .when('/privacyPolicy', {
+                    templateUrl: '/partials/privacy.html'
+                })
+                .when('/sitemap', {
+                    templateUrl: '/partials/sitemap.html'
+                })
+                .otherwise({ redirectTo: '/' });
+
+                $locationProvider.html5Mode({ enabled: true, requireBase: false });
+            }]);
